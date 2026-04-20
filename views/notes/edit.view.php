39288a753f5bc2cbeb3a3731037e06eea5e66b60
sWARNAV <swarnavsarkar20@protonmail.com>
2026-04-21 01:52:55 +0530

    Fix: Delete button on Edit page

--- views/notes/edit.view.php
+++ views/notes/edit.view.php
@@ -5,6 +5,11 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
+    <form id="del-button" method="POST" action="/note">
+      <input type="hidden" name="_method" value="DELETE">
+      <input type="hidden" name="id" value="<?= $note["id"]; ?>">
+    </form>
+
     <form method="POST" action="/note">
       <input type="hidden" name="_method" value="PATCH">
       <input type="hidden" name="id" value="<?= $note["id"]; ?>">
@@ -21,10 +26,22 @@
                 <p class="text-red-900 font-bold text-xs mt-2"><?= $errors['body'] ?></p>
               <?php endif; ?>
 
-              <div class=" mt-6 flex items-center justify-end gap-x-6">
-                <a href="/note?id=<?= $note["id"]; ?>" class="text-sm/6 font-semibold text-white">Cancel</a>
-                <button type="submit"
-                  class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Update</button>
+
+              <div class="mt-6 flex items-center justify-between gap-x-6">
+                <!-- =========================================
+                        DELETE BUTTON
+                    =========================================
+                -->
+                  <button form="del-button" type="submit"
+                    class="rounded-md border-b-4 border-b-purple-600 hover:border-b-red-600 bg-black px-3 py-2 text-sm text-purple-600 hover:text-red-600 ">Delete</button>
+
+                <!-- =END= -->
+
+                <div class="flex items-center justify-end gap-x-6">
+                  <a href="/note?id=<?= $note["id"]; ?>" class="text-sm/6 font-semibold text-white">Cancel</a>
+                  <button type="submit"
+                    class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Update</button>
+                </div>
               </div>
 
             </div>
@@ -32,19 +49,7 @@
     </form>
 
 
-    <div class="mt-6 flex items-center justify-between gap-x-6">
-      <!-- =========================================
-            DELETE BUTTON
-       =========================================
-       -->
-      <form class="flex items-center justify-start" method="POST" action="/note">
-        <input type="hidden" name="_method" value="DELETE"> <!-- To send different types of requests. -->
-        <input type="hidden" name="id" value="<?= $note["id"]; ?>"> <!-- To send the id of the current note -->
-        <button
-          class="rounded-md border-b-4 border-b-purple-600 hover:border-b-red-600 bg-black px-3 py-2 text-sm text-purple-600 hover:text-red-600 ">Delete</button>
-      </form>
-      <!-- =END= -->
-    </div>
+
 
 
   </div>
