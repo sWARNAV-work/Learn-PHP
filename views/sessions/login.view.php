c9a83c8cc1eaf6b8452d4d734bd1472ca206cdb4
sWARNAV <swarnavsarkar20@protonmail.com>
2026-05-15 13:35:38 +0530

    Typo

--- views/sessions/login.view.php
+++ views/sessions/login.view.php
@@ -20,7 +20,7 @@ require base_path("views/partials/nav.php");
                     <div>
                         <label for="email" class="block text-sm/6 font-medium text-gray-100">Email address</label>
                         <div class="mt-2">
-                            <input id="email" type="email" name="email" autocomplete="email" value = "<?= old("email") ?>"
+                            <input id="email" type="email" name="email" autocomplete="email" value = "<?= old("email"); ?>"
                                 class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
 
                             
